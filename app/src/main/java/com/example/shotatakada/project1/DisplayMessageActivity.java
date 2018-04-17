067628df0d9c2c1829b026f6a73a8a3de0087364
Stakada <st.shota96@gmail.com>
2018-04-17 07:49:13 -0700

    Added a logcat debugger

---
+++ app/src/main/java/com/example/shotatakada/project1/DisplayMessageActivity.java
@@ -0,0 +1,36 @@
+package com.example.shotatakada.project1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+    private static final String TAG = DisplayMessageActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            Log.d(TAG, "onCreate() Restoring previous state");
+            /* restore state */
+        } else {
+            Log.d(TAG, "onCreate() No saved state available");
+            /* initialize app */
+        }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Hello " + message);
+    }
+}
